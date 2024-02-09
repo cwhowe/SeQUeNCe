fbe51198a693a0652f773112d769eb12b1d5d00f
Connor Howe <howe0427@umn.edu>
2024-02-09 12:43:45 -0600

    Working version, make sure to change the paths specified in files to match host machine paths

---
+++ run_parallel.py
@@ -0,0 +1,26 @@
+import multiprocessing
+import papermill as pm
+
+def run_script(iteration):
+    notebook_path = 'example/Multi_Node_Framework_WORKING.ipynb'
+    output_path = f'example/outputs/Multi_Node_Framework_WORKING_output_{iteration}.ipynb'
+
+    pm.execute_notebook(
+        notebook_path,
+        output_path,
+        parameters={'run_cycle': iteration}
+    )
+
+if __name__ == "__main__":
+    processes = []
+
+    for i in range(5):
+        #print(f"Creating process for iteration {i}")
+        p = multiprocessing.Process(target=run_script, args=(i,))
+        p.start()
+        processes.append(p)
+        #print(f"Process for iteration {i} started")
+
+    for process in processes:
+        process.join()
+        #print("Process joined")
